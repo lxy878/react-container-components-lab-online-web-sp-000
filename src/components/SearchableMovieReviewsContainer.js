8985d6d14173f4e4a8172d8a7c384588c9c294a0
Xiaoyi Liu <lxy878@gmail.com>
2020-11-08 15:09:18 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,30 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component{
+    state = {
+        searchTerm: '',
+        reviews: []
+    }
+
+    handleChange = e =>{
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleClick = e =>{
+        e.preventDefault()
+        fetch(URL+`&query=${this.state.searchTerm}`).then(resp=>resp.json()).then(reviews=>this.setState({reviews: reviews.results}))
+    }
+
+    render(){
+        return (<div>
+            <form onSubmit={this.handleClick}>
+                <input id='searchTerm' onChange={this.handleChange} value={this.state.searchTerm}/>
+                <input type='submit' />
+            </form>
+            <MovieReviews reviews={this.state.reviews}/>
+        </div>)
+    }
+}
