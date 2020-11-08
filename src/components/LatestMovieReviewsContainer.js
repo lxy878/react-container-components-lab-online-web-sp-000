a22dab81b7b7c6aae36b262466ab4dce2beb41be
Xiaoyi Liu <lxy878@gmail.com>
2020-11-08 15:09:52 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,20 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component{
+    state={
+        reviews: []
+    }
+
+    componentDidMount(){
+        fetch(URL).then(resp=>resp.json()).then(reviews => this.setState({reviews: reviews.results}))
+    }
+
+    render() {
+        return (
+            <div className='latest-movie-reviews'>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
-// Code MovieReviews Here
+import React from 'react'
+
+
+const MovieReviews = ({reviews}) => (
+    <div className='review-list'>
+        {reviews.map((rev, index)=> review(rev, index))}
+    </div>
+)
+
+const review = ({display_title, multimedia}, index) => (
+    <div key={index.toString()} className='review'>
+        <h3>{display_title}</h3>
+        <img src={multimedia.src} width={multimedia.width} height={multimedia.height}></img>
+    </div>
+)
+
+export default MovieReviews
