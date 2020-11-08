348a6a4e19564567ec97d7f54162beccedd00268
Xiaoyi Liu <lxy878@gmail.com>
2020-11-08 15:07:42 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,12 +3,12 @@ import React from 'react'
 
 const MovieReviews = ({reviews}) => (
     <div className='review-list'>
-        {reviews.map(rev=> review(rev))}
+        {reviews.map((rev, index)=> review(rev, index))}
     </div>
 )
 
-const review = ({display_title, multimedia}) => (
-    <div className='review'>
+const review = ({display_title, multimedia}, index) => (
+    <div key={index.toString()} className='review'>
         <h3>{display_title}</h3>
         <img src={multimedia.src} width={multimedia.width} height={multimedia.height}></img>
     </div>
