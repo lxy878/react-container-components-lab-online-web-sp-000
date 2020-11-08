ccda1df33d0352c858853362c3ff21cdd333bfdd
Xiaoyi Liu <lxy878@gmail.com>
2020-11-08 15:05:11 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
-// Code MovieReviews Here
+import React from 'react'
+
+
+const MovieReviews = ({reviews}) => (
+    <div className='review-list'>
+        {reviews.map(rev=> review(rev))}
+    </div>
+)
+
+const review = ({display_title, multimedia}) => (
+    <div className='review'>
+        <h3>{display_title}</h3>
+        <img src={multimedia.src} width={multimedia.width} height={multimedia.height}></img>
+    </div>
+)
+
+export default MovieReviews
